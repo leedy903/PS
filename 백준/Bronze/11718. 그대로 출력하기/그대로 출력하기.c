f902a6fe99293bd0a6b6171a8ad07057e25c420d
leedy903 <leedy903@naver.com>
2025-02-20 09:36:02 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	
+	char sentences[101];
+	
+	// we don't use scanf() because we have 'space' in the sentences.
+	while (fgets(sentences, sizeof(sentences), stdin))
+		printf("%s", sentences);
+	return 0;
+}
