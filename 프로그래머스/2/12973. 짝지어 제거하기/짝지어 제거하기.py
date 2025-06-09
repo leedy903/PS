ac5045b6e760134c698bdb52f1075247e010d6de
leedy903 <leedy903@naver.com>
2025-06-09 16:21:58 +0900

    [level 2] Title: 짝지어 제거하기, Time: 133.39 ms, Memory: 14.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in range(len(s)):
+        if stack and stack[-1] == s[i]:
+            stack.pop()
+        else:
+            stack.append(s[i])
+
+    if len(stack) == 0:
+        answer = 1
+    return answer
