b9101d451d4badb9231446b245a2dd04aa5397b6
leedy903 <leedy903@naver.com>
2025-06-09 16:40:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.71 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0
+    dp = [0 for _ in range(n + 2)]
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    answer = dp[n] % 1234567
+    return answer
