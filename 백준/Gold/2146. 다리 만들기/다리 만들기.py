c7db0e82833a9a8c34b264f24884001858cc7007
leedy903 <leedy903@naver.com>
2025-10-26 01:31:33 +0900

    [Gold III] Title: 다리 만들기, Time: 144 ms, Memory: 116100 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -7,6 +7,7 @@ dx = [0, -1, 0, 1]
 
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
+borders = []
 
 continent_id = 0
 visited = [[False] * n for _ in range(n)]
@@ -19,6 +20,7 @@ for y in range(n):
             matrix[y][x] = continent_id
             while deq:
                 cy, cx = deq.popleft()
+                is_border = False
                 for i in range(4):
                     ny, nx = cy + dy[i], cx + dx[i]
                     if 0 <= ny < n and 0 <= nx < n:
@@ -26,21 +28,24 @@ for y in range(n):
                             visited[ny][nx] = True
                             matrix[ny][nx] = continent_id
                             deq.append((ny, nx))
+                        elif matrix[ny][nx] == 0:
+                            is_border = True
+                if is_border:
+                    borders.append([cy, cx])
+
+deq = deque()
+dist = [[-1] * n for _ in range(n)]
+for y, x in borders:
+    deq.append((y, x))
+    dist[y][x] = 0
 
 answer = float('inf')
-deq = deque()
-
-dist = [[-1] * n for _ in range(n)]
-for y in range(n):
-    for x in range(n):
-        if matrix[y][x] > 0:
-            deq.append((y, x))
-            dist[y][x] = 0
 
 while deq:
     y, x = deq.popleft()
     for i in range(4):
-        ny, nx = y + dy[i], x + dx[i]
+        ny = y + dy[i]
+        nx = x + dx[i]
         if 0 <= ny < n and 0 <= nx < n:
             if matrix[ny][nx] == 0:
                 matrix[ny][nx] = matrix[y][x]
