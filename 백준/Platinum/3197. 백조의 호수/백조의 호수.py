0cf1eb663d0631c1e3d27f0833a70efcda35b355
leedy903 <leedy903@naver.com>
2025-12-29 11:48:08 +0900

    [Platinum V] Title: 백조의 호수, Time: 1896 ms, Memory: 312080 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -1,93 +1,72 @@
-import sys
 from collections import deque
-
+import sys
 input = sys.stdin.readline
 
-def find_parent(x):
-    if x != parents[x]:
-        parents[x] = find_parent(parents[x])
-    return parents[x]
+def solve():
+    R, C = map(int, input().split())
+    lake = [list(input().rstrip()) for _ in range(R)]
 
-def unoin(a, b):
-    a = find_parent(a)
-    b = find_parent(b)
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
 
-    if a < b:
-        parents[b] = a
-    else:
-        parents[a] = b
+    water_q = deque()
+    swans = []
 
-answer = 1
-R, C = map(int, input().split())
-matrix = [list(input().strip()) for _ in range(R)]
+    for y in range(R):
+        for x in range(C):
+            if lake[y][x] != 'X':
+                water_q.append((y, x))
+            if lake[y][x] == 'L':
+                swans.append((y, x))
 
-dy = [-1, 0, 1, 0]
-dx = [0, -1, 0, 1]
+    sy, sx = swans[0]
+    ty, tx = swans[1]
 
-visited = [[False for _ in range(C)] for _ in range(R)]
-parents = [i for i in range(R * C)]
-boarder_line = set()
-swans = []
+    swan_q = deque([(sy, sx)])
+    next_swan_q = deque()
 
-for y in range(R):
-    for x in range(C):
-        if matrix[y][x] == 'L':
-            swans.append([y, x])
-        if not visited[y][x] and matrix[y][x] in ('.', 'L'):
-            visited[y][x] = True
-            deq = deque([[y, x]])
-            while deq:
-                cy, cx = deq.popleft()
-                for i in range(4):
-                    ny = cy + dy[i]
-                    nx = cx + dx[i]
-                    if 0 <= ny < R and 0 <= nx < C and not visited[ny][nx]:
-                        if matrix[ny][nx] in ('.', 'L'):
-                            cur_id = find_parent(cy * C + cx)
-                            next_id = find_parent(ny * C + nx)
-                            unoin(cur_id, next_id)
-                            visited[ny][nx] = True
-                            deq.append([ny, nx])
-                        if matrix[ny][nx] == 'X':
-                            boarder_line.add((cy, cx))
+    water_visited = [[False]*C for _ in range(R)]
+    swan_visited = [[False]*C for _ in range(R)]
 
-boarder_line = deque(list(boarder_line)[:])
+    water_visited[sy][sx] = True
+    swan_visited[sy][sx] = True
 
-for day in range(1, R * C + 1):
-    next_boarder_line = set()
-    while boarder_line:
-        y, x = boarder_line.popleft()
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < R and 0 <= nx < C:
-                if matrix[ny][nx] == 'X':
-                    next_boarder_line.add((ny, nx))
+    day = 0
 
-    next_boarder_line = list(next_boarder_line)
+    while True:
+        while swan_q:
+            y, x = swan_q.popleft()
+            if (y, x) == (ty, tx):
+                print(day)
+                return
 
-    for y, x in next_boarder_line:
-        matrix[y][x] = '.'
+            for d in range(4):
+                ny, nx = y + dy[d], x + dx[d]
+                if 0 <= ny < R and 0 <= nx < C and not swan_visited[ny][nx]:
+                    swan_visited[ny][nx] = True
+                    if lake[ny][nx] == 'X':
+                        next_swan_q.append((ny, nx))
+                    else:
+                        swan_q.append((ny, nx))
 
-    for y, x in next_boarder_line:
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < R and 0 <= nx < C:
-                if matrix[ny][nx] in ('.', 'L'):
-                    cur_id = find_parent(y * C + x)
-                    next_id = find_parent(ny * C + nx)
-                    unoin(cur_id, next_id)
+        next_water_q = deque()
+        while water_q:
+            y, x = water_q.popleft()
+            for d in range(4):
+                ny, nx = y + dy[d], x + dx[d]
+                if 0 <= ny < R and 0 <= nx < C and not water_visited[ny][nx]:
+                    water_visited[ny][nx] = True
+                    if lake[ny][nx] == 'X':
+                        lake[ny][nx] = '.'
+                        next_water_q.append((ny, nx))
+                    else:
+                        water_q.append((ny, nx))
 
-    s1 = swans[0]
-    s2 = swans[1]
-    s1_id = find_parent(s1[0] * C + s1[1])
-    s2_id = find_parent(s2[0] * C + s2[1])
-    
-    if s1_id == s2_id:
-        answer = day
-        break
+        swan_q = next_swan_q
+        water_q = next_water_q
+        next_swan_q = deque()
 
-    boarder_line = deque(next_boarder_line[:])
+        day += 1
 
-print(answer)
+if __name__ == "__main__":
+    solve()
