f6d508dfa816d0a15ddeb4876e883365c99a6de2
leedy903 <leedy903@naver.com>
2026-03-27 01:22:47 +0900

    [Silver I] Title: 경로 찾기, Time: 224 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,25 +1,13 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-
 N = int(input())
 adj_matrix = [list(map(int, input().strip().split())) for _ in range(N)]
 
-reachable = [[0 for _ in range(N)] for _ in range(N)]
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if adj_matrix[i][k] and adj_matrix[k][j]:
+                adj_matrix[i][j] = 1
 
 for i in range(N):
-    visited = [0] * N
-    deq = deque([i])
-
-    while deq:
-        cur = deq.popleft()
-        for next in range(N):
-            if adj_matrix[cur][next] == 1 and not visited[next]:
-                visited[next] = 1
-                deq.append(next)
-    
-    for j in range(N):
-        reachable[i][j] = visited[j]
-    
-for i in range(N):
-    print(*reachable[i])
+    print(*adj_matrix[i])
