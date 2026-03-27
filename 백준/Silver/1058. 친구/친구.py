6b07408b249462cbe27b6204ec0dd76b599631b0
leedy903 <leedy903@naver.com>
2026-03-28 01:07:50 +0900

    [Silver II] Title: 친구, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -4,24 +4,21 @@ input = sys.stdin.readline
 N = int(input())
 friends = [list(input().strip()) for _ in range(N)]
 
-dist = [[float('inf')] * N for _ in range(N)]
-
-for i in range(N):
-    for j in  range(N):
-        if friends[i][j] == 'Y':
-            dist[i][j] = 1
-
-for k in range(N):
-    for i in range(N):
-        for j in range(N):
-            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-    
 answer = 0
 for i in range(N):
     cnt = 0
     for j in range(N):
-        if i != j and dist[i][j] <= 2:
+        if i == j:
+            continue
+
+        if friends[i][j] == 'Y':
             cnt += 1
+            continue
+
+        for k in range(N):
+            if friends[i][k] == 'Y' and friends[k][j] == 'Y':
+                cnt += 1
+                break
     answer = max(answer, cnt)
 
 print(answer)
